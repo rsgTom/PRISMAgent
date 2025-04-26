bb625cdf477caaf1e6d1c671845131644e9a3e4a
rsgTom <97708239+rsgTom@users.noreply.github.com>
2025-04-25 23:30:17 -0400

    Update tools __init__.py to include code_interpreter and web_search tools

--- src/PRISMAgent/tools/__init__.py
+++ src/PRISMAgent/tools/__init__.py
@@ -5,6 +5,8 @@ This module provides OpenAI function tools that can be used by agents.
 
 from .spawn import spawn_agent
 from .factory import tool_factory, list_available_tools
+from .code_interpreter import code_interpreter, install_package
+from .web_search import web_search, fetch_url
 from PRISMAgent.util import get_logger
 
 # Get a logger for this module
@@ -12,4 +14,12 @@ logger = get_logger(__name__)
 
 logger.debug("PRISMAgent.tools module initialized")
 
-__all__ = ["spawn_agent", "tool_factory", "list_available_tools"]
+__all__ = [
+    "spawn_agent", 
+    "tool_factory", 
+    "list_available_tools",
+    "code_interpreter",
+    "install_package",
+    "web_search",
+    "fetch_url"
+]
